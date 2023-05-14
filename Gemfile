7f7dc90778190a013666070abf8342df156ce589
Abraham <abkuri88@gmail.com>
2023-05-14 21:46:11 +0000

    Adds openai gem config

--- Gemfile
+++ Gemfile
@@ -95,3 +95,4 @@ group :test do
 end
 
 gem "tailwindcss-rails", "~> 2.0"
+gem 'ruby-openai'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_14_205645) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_14_213727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_14_205645) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "result"
   end
 
   create_table "searches", force: :cascade do |t|
