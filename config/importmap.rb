4c62e375258c7b9f353829ac9ee20ae6a17993b0
Abraham <abkuri88@gmail.com>
2023-05-18 22:19:21 +0000

    Adds prism

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin 'application', preload: true
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin "prismjs", to: "https://ga.jspm.io/npm:prismjs@1.29.0/prism.js"
+pin_all_from 'app/javascript/controllers', under: 'controllers'
